3b52028813c6fdb0f5014c2a2e3b3bb5c5ba4e9b
Oumar <oumar.youssouf0605@gmail.com>
2021-06-06 13:43:41 +0200

    Adding script to background image

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const quote = document.getElementById('quote');
 const getQuote = ()=> {
 
     fetch('https://api.quotable.io/random')
-    .then(response => response.json())
+    .then((res) => res.json())
     .then(data => {
         quote.innerHTML = (`${data.content} — ${data.author}`)
     })
+
+    fetch('https://picsum.photos/1600/1000')
+    .then((res) => {
+     document.getElementById('pic').innerHTML = 
+     `<img src=${res.url} />`
+    })
+    
 };
 
 quote.addEventListener('click', ()=> getQuote());
