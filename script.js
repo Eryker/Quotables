b745df3283ae04726c4856b1d9f5b5964ca149e9
Oumar <oumar.youssouf0605@gmail.com>
2021-06-06 12:30:01 +0200

    Adding script from github API

---
+++ script.js
@@ -0,0 +1,7 @@
+const quote = document.getElementById('quote');
+
+fetch('https://api.quotable.io/random')
+  .then(response => response.json())
+  .then(data => {
+    console.log(`${data.content} —${data.author}`)
+  })
